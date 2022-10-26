ab9f986da16b1b648251ded86de7e4ae642d3fea
Mauricio Montoya <mmontoyaabadia@gmail.com>
2022-10-26 15:35:49 -0500

    Linea hover y active listo

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+const navLink = document.querySelectorAll('.link');   
+
+function linkAction(){
+    /*Active link*/
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    /*Remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')*/
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
